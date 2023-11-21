371d59e6fa26e1c699ef7384eb0a5405fdb2c535
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2023-11-21 15:01:37 +0700

    First commit, modified database.py and project_manage.py by defining the initializing and login function after that i have tested that it work.

--- project_manage.py
+++ project_manage.py
@@ -1,56 +1,65 @@
 # BEGIN part 1
 
 # import database module
+from database import ReadCSV, DB, Table
+import random
+# define a function called initializing
 
-# define a funcion called initializing
+
+def generate_random_password():
+    # return ''.join(random.choices(string.digits, k=4))
+    password = ''
+    for i in range(1, 5):
+        password += str(random.randint(0, 9))
+    return password
+
 
 def initializing():
-    pass
-
-# here are things to do in this function:
-
-    # create an object to read an input csv file, persons.csv
-
-    # create a 'persons' table
-
-    # add the 'persons' table into the database
-
-    # create a 'login' table
-
-    # the 'login' table has the following keys (attributes):
-        # person_id
-        # username
-        # password
-        # role
-
-    # a person_id is the same as that in the 'persons' table
-
-    # let a username be a person's fisrt name followed by a dot and the first letter of that person's last name
-
-    # let a password be a random four digits string
-
-    # let the initial role of all the students be Member
-
-    # let the initial role of all the faculties be Faculty
-
-    # create a login table by performing a series of insert operations; each insert adds a dictionary to a list
-
-    # add the 'login' table into the database
+    my_db = DB()
+    read_csv = ReadCSV('persons.csv')
+    persons = read_csv.read_csv()
+    persons_table = Table('persons', persons)
+    my_db.insert(persons_table)
+    login = []
+    login_table = Table('login', login)
+    for i in persons_table.table:
+        login_dict = {}
+        person_id = i.get('ID', '')
+        username = i.get('fist', '') + '.' + i.get('last', '')[0]
+        password = generate_random_password()
+        if i.get('type', '') == 'faculty':
+            role = 'Faculty'
+        else:
+            role = 'Member'
+        login_dict['person_id'] = person_id
+        login_dict['username'] = username
+        login_dict['password'] = password
+        login_dict['role'] = role
+        login.append(login_dict)
+    my_db.insert(login_table)
+    return my_db
 
 # define a funcion called login
 
-def login():
-    pass
 
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [person_id, role] if valid, otherwise returning None
+def login():
+    my_db = initializing()
+    login_search = my_db.search('login')
+    print(login_search)
+    username = input('Username: ')
+    password = input('Password: ')
+    for i in login_search.table:
+        if i['username'] == username and i['password'] == password:
+            personal_id = i.get('person_id', '')
+            role = i.get('role', '')
+            return [personal_id, role]
+        return None
 
 # make calls to the initializing and login functions defined above
 
 initializing()
 val = login()
+print(val)
 
 # END part 1
 
